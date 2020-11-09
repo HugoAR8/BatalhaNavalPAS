efed847bcd1f5dadabbe8afe092842305da4c44e
HugoAR8 <hugoar@ucl.br>
2020-11-09 11:21:21 -0300

    Preparação pro sistema de tiro

--- BatalhaNaval/Assets/Ship.cs
+++ BatalhaNaval/Assets/Ship.cs
@@ -4,10 +4,25 @@ using UnityEngine;
 
 public class Ship : MonoBehaviour
 {
+    protected float life = 0;
+    [SerializeField] protected float maxLife;
+
+    [SerializeField] protected float speed;
+    protected float damagedSpeed;
+
+    [SerializeField] protected int smallCannonQtd;
+    [SerializeField] protected int mediumCannonQtd;
+    [SerializeField] protected int bigCannonQtd;
+    //Criar listas de cada tipo de canhão.
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        life = maxLife;
+        damagedSpeed = speed / 2;
     }
 
     // Update is called once per frame
@@ -15,4 +30,13 @@ public class Ship : MonoBehaviour
     {
         
     }
+
+    public virtual void underAttack(float damage)
+    {
+    }
+
+    public virtual void Attack()
+    {
+    }
+
 }
